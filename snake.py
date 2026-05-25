a4b22d68d4e1edfbdb69e26ee7f7782eaa1f450f
Christianib003 <christianib003@gmail.com>
2026-05-25 10:41:49 +0200

    implement changing direction of snake using arrow keys

--- snake.py
+++ snake.py
@@ -3,6 +3,10 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
@@ -10,7 +14,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
-    
+        self.head = self.segments[0]
     
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -25,4 +29,20 @@ class Snake:
             prev_seg_x = self.segments[seg_num - 1].xcor()
             prev_seg_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(prev_seg_x, prev_seg_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+    
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+    
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
