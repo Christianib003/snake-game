6b79c557436ffc8e3475164111b1a28d30c26bc2
Christianib003 <christianib003@gmail.com>
2026-05-25 10:20:47 +0200

    refactor snake related actions into class Snake

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 
 
@@ -8,26 +9,14 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-
-for position in starting_positions:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
 
 is_game_active = True
 while is_game_active:
     screen.update()
     time.sleep(0.1)
     
-    for seg_num in range(len(segments) - 1, 0, -1):
-        prev_seg_x = segments[seg_num - 1].xcor()
-        prev_seg_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(prev_seg_x, prev_seg_y)
-    segments[0].forward(20)
+    snake.move()
 
 
 screen.exitonclick()
