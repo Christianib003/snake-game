8f32a0ff010ff53a20c1c755af70a467f8c17b7a
Christianib003 <christianib003@gmail.com>
2026-05-25 09:53:24 +0200

    implement snake movement in one direction

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from turtle import Turtle, Screen
+import time
 
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+segments = []
 
 for position in starting_positions:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(position)
+    segments.append(new_segment)
+
+is_game_active = True
+while is_game_active:
+    screen.update()
+    time.sleep(0.1)
+    for segment in segments:
+        segment.forward(20)
 
 
 screen.exitonclick()
