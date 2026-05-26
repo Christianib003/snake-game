e991be91d5b1b5b1e3f64c33b04f2acc2b04dc76
Christianib003 <christianib003@gmail.com>
2026-05-26 07:49:21 +0200

    create class Scoreboard to track and display user score

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 
@@ -12,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -27,6 +29,7 @@ while is_game_active:
     
     if snake.head.distance(food) < 20:
         food.refresh()
+        scoreboard.increase_score()
 
 
 screen.exitonclick()
