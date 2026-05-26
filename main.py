184a4314956d07a10ea939d1870c6e68af2f5c6f
Christianib003 <christianib003@gmail.com>
2026-05-26 08:06:18 +0200

    implement detection with tail

--- main.py
+++ main.py
@@ -35,6 +35,13 @@ while is_game_active:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         is_game_active = False
         scoreboard.game_over()
+    
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
